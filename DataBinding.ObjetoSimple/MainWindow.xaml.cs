dfe59129fc8038efe466b62f97bce6a28d3ded80
Daniel Pelagio Puch <19470231@campeche,tecnm.mx>
2022-01-10 18:12:41 -0600

    Sincroniza el modelo de datos y la interfaz de usuario con el mecanismo de manejo de eventos.

---
+++ DataBinding.ObjetoSimple/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DataBinding.ObjetoSimple.Model;
+using System;
+using System.Windows;
+
+
+namespace DataBinding.ObjetoSimple
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Evento evento;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            evento = new Evento()
+            {
+                Title = "Reunión de trabajo",
+                Date = new DateTime(2021, 10, 12)
+            };
+            UpdateUI();
+
+        }
+
+        private void UpdateUI()
+        {
+            eventoTextBox.Text = evento.Title;
+            fechaDatePicker.SelectedDate = evento.Date;
+        }
+
+        private void modificarEventoButton_Click(object sender, RoutedEventArgs e)
+        {
+            evento.Title = evento.Title.ToUpper();
+            evento.Date = evento.Date.AddDays(1);
+            UpdateUI();
+        }
+    }
+}
---
+++ DataBinding.ObjetoSimple/Model/Evento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DataBinding.ObjetoSimple.Model
+{
+    public class Evento
+    {
+        private string _Title;
+        private DateTime _Date;
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (value != _Title)
+                {
+                    _Title = value;
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get => _Date;
+            set
+            {
+                if (value != _Date)
+                {
+                    _Date = value;
+                }
+            }
+        }
+
+
+    }
+}
